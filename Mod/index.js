c850b040fc15bf9623be6c081ef7ac177dc4d54a
metrics-as-a-service <62787544+metrics-as-a-service@users.noreply.github.com>
2024-11-30 17:23:03 +0000

    Add files via upload

---
+++ Mod/index.js
@@ -0,0 +1,3 @@
+"use strict"
+import { f1 } from "./one.js"
+f1("it works")
---
+++ Mod/one.js
@@ -0,0 +1,5 @@
+export function f1(message) {
+    const e = document.createElement("h2")
+    e.textContent = message
+    document.body.append(e)
+}
